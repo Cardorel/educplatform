14777c99cb3045c3a39364da4aaaba225b66295c
Cardorel <54881922+Cardorel@users.noreply.github.com>
2023-01-02 19:53:31 +0100

    join container

--- src/components/joinUs/JoinContainer.jsx
+++ src/components/joinUs/JoinContainer.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import BigLogo from "../logo/BigLogo";
 import ArrowBack from "../../assets/svg/arrowLeflLong.svg";
 import { Image } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function JoinContainer({ children }) {
+  const navigate = useNavigate();
   return (
     <div className="join">
       <div className="join-logo">
         <BigLogo />
-        <button className="arrow-back">
+        <button className="arrow-back" onClick={() => navigate("/")}>
           <Image src={ArrowBack} alt="arow" />
           Назад до головної
         </button>
